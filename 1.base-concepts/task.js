03b337703fdd5bc4f6fc90c27c71126d0ab24b6a
KaPuHKa <p4elkaachen@mail.ru>
2022-06-07 21:42:56 +0200

    1. task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,14 @@
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  "use strict";
+  let arr = [];
+  let d = b*b - 4*a*c;
+  if (d>0){
+    arr = [(-b+Math.sqrt(d))/(2*a), (-b-Math.sqrt(d))/(2*a) ];
+  } else if (d === 0){
+    arr = [-b/(2*a)];
+  }
+  
   return arr; // array
 }
 
