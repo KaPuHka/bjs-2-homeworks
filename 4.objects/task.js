dcba68a9abd407d16c91cba088ebbf21e4c5a79b
KaPuHka <p4elkaachen@mail.ru>
2022-07-08 09:43:11 +0300

    objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
+  this.marks = [];
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark){
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks){
+  marks.forEach(mark => {
+    this.addMark(mark);
+  });
+}
+
+
+Student.prototype.getAverage = function (){
+  let sum = 0;
+  this.marks.forEach(mark=> {
+    sum += mark;
+  });
+  return (this.marks.length >0) ? sum/this.marks.length : 0;
+}
+
+Student.prototype.exclude = function (reason){
+    this.excluded = reason;
+    delete this.marks;
+    delete this.subject;
+}
